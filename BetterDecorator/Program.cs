5fc22845c2becb2a1c093def49f1da834a1bdb04
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-12 15:58:30 +0000

    added chars multiplied by int

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+
+namespace BetterDecor
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s = args[0];
+            char dec = char.Parse(args[1]);
+            int n = int.Parse(args[2]);
+            
+                
+
+         Console.WriteLine(Decor(args[0], dec, n));
+        }
+        private static string Decor(string s, char dec, int n)
+            {
+                string decs = "";
+                int i;
+                for (i = 0; i < n; i++)
+                    {
+                        decs += dec;
+                    }
+                return $"{decs} {s} {decs}"; 
+            }
+    }
+}
